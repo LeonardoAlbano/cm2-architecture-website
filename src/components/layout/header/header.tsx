b93fb985f17e1cbbb3c0c9bbef0ac841be3ec01e
leonardoalbano <leonardoalbanom@yahoo.com.br>
2025-06-08 18:51:56 -0300

    create config base

---
+++ src/components/layout/header/header.tsx
@@ -0,0 +1,24 @@
+import ActiveLink from "@/components/active-link/active-link";
+import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="fixed top-0 z-50 w-full border-b boder-white/10 bg-background/95 backdrop-blur supports-[backdrop-fill]:bg-background/60">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 ls:px-8">
+        <div className="flex h-16 items-center justify-between">
+          <LogOut />
+
+          <nav className="flex items-center gap-6">
+            <ActiveLink href="/">Início</ActiveLink>
+            <ActiveLink href="/blog">Blog</ActiveLink>
+            <Button variant="secondary" asChild>
+              <Link href="/comecar">Começar</Link>
+            </Button>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
---
+++ src/templates/landing-page/landing-page.tsx
@@ -0,0 +1,11 @@
+export default function LandingPage() {
+  return (
+    <article className="flex flex-col gap-10 md:gap-20 border border-red-600">
+      <div>Hero</div>
+      <div>Hero</div>
+      <div>Hero</div>
+      <div>Hero</div>
+      <div>Hero</div>
+    </article>
+  );
+}
